37840341d0d8431635b94a23b4d53bac43cbe95c
Milalien <karkkimilla@gmail.com>
2023-11-20 13:57:20 +0200

    ..

--- healthapp/src/app/app.module.ts
+++ healthapp/src/app/app.module.ts
@@ -8,6 +8,18 @@ import { ProgramsComponent } from './programs/programs.component';
 import { ProfileComponent } from './profile/profile.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { FooddiaryComponent } from './fooddiary/fooddiary.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -16,11 +28,22 @@ import { FooddiaryComponent } from './fooddiary/fooddiary.component';
     ProgramsComponent,
     ProfileComponent,
     CalendarComponent,
-    FooddiaryComponent
+    FooddiaryComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
