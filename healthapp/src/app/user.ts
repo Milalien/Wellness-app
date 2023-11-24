887f38b04b02ba069a8265229f6cafca7797b4d7
st1nde <122520773+st1nde@users.noreply.github.com>
2023-11-24 17:07:40 +0200

    24112023v2
    Added user class

---
+++ healthapp/src/app/user.ts
@@ -0,0 +1,19 @@
+export class User {
+    userName: string = "";
+    firstName: string = "";
+    lastName: string = "";
+    email: string = "";
+    password: string = "";
+    passwordConfirm: string = "";
+
+    constructor() {
+        this.userName = "";
+        this.firstName = "";
+        this.lastName = "";
+        this.email = "";
+        this.password = "";
+        this.passwordConfirm = "";
+    }
+
+
+}
