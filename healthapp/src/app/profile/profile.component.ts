52c4171a4dcc7dc2adfa58e08ff334ebc629b1d0
st1nde <122520773+st1nde@users.noreply.github.com>
2023-11-24 16:59:53 +0200

    24112023
    Profile template Done

--- healthapp/src/app/profile/profile.component.ts
+++ healthapp/src/app/profile/profile.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
+  itemImageUrl = 'https://i2-prod.dailystar.co.uk/incoming/article31219862.ece/ALTERNATES/s810/3_staapp_283253722_737353524288989_2288895842199857210_n_1080.jpg';
+
 }
